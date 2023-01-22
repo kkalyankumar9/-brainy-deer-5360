f3a58df1df81243a7db077b010f38e9e346d5fee
kkalyankumar9 <kummarikalyan010@gmail.com>
2023-01-22 12:37:15 +0530

    .

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 let image=document.querySelector(".poster");
 let translate=0;
 setInterval(()=>{
-    if(translate>=0 && translate<400){
+    if(translate>=0 && translate<500){
         translate+=100
     }
     else{
         translate=0
     }
 
-    image.style.transform="translateX(${~translate}%)"
+    image.style.transform=`translateX(${~translate}%)`
     
-},2000)
+},5000)
+ 
+function right(){
+    if(translate<500){
+        translate+=100
+        image.style.transtion="1s";
+        image.style.transform=`translateX(${-translate}%)`
+
+    }
+}
+
+function left(){
+    if(translate>0){
+        translate-=100
+        image.style.transtion="1s";
+        image.style.transform=`translateX(${-translate}%)`
+
+    }
+}
+
